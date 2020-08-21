7f94126d95ed5441e45f82ed37f0e2710a1338dd
peedroluiss <pedroluisbarillas@gmail.com>
2020-08-20 18:15:36 -0600

    login arreglado

--- Models/Calendario/models.py
+++ Models/Calendario/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 
 class Calendario(models.Model):
-    id_calendario= models.AutoField(primary_key=True)
+    id_ca = models.AutoField(primary_key=True)
     Descripcion =  models.CharField(max_length=30)
 
 
-
-
     def __str__(self):
-        return '{} {}'.format(self.Descripcion)
+        return '{} {}'.format(self.id_ca)
